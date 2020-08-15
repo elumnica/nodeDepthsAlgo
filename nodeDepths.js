60e7b63bbc6886e2a3d602c5a64f232575372c6c
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-08-14 22:51:34 -0400

    second solution with time/space complexity

--- nodeDepths.js
+++ nodeDepths.js
@@ -37,3 +37,25 @@ class BinaryTree {
       this.right = null;
     }
   }
+
+
+
+
+  //Solution Two
+  //Average case: when the tree is balanced
+  //O(n) time | O(h) space - where is n is the number of nodes in
+  // the Binary Tree and h is the height of the Binary Tree
+
+  function nodeDepths(root, depth = 0){
+      if (root === null) return 0;
+      return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
+  }
+
+// This is the class of the input binary tree.
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
